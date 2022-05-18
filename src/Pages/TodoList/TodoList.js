eeea810e211152df121d59fd6c0889b0bdc99fc1
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-19 01:23:44 +0600

    navbar added

---
+++ src/Pages/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div>
+            <h2 className='text-red-500'>todolist</h2>
+        </div>
+    );
+};
+
+export default TodoList;
